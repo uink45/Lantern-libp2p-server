e7659e99182bde91388f783e254826e1b6fdbc85
uink45 <79078981+uink45@users.noreply.github.com>
2022-03-25 09:55:11 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const PORT = 9000;
+const PORT = 6000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
